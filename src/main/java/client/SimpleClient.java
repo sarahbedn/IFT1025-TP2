94df8b764464f3d1cbc8f0d4e4678085b80c3ffd
Sarah Bedn <sarah.bedn@outlook.com>
2023-04-17 17:58:28 -0400

    Tache 2: Implementation des methods pour l'inscription at charger les cours dans la classe SimpleClient

--- src/main/java/client/SimpleClient.java
+++ src/main/java/client/SimpleClient.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.List;
 import java.util.Scanner;
+
+import server.models.Course;
+import server.models.RegistrationForm;
+import server.Server;
 
 /**
  * Une classe représentant un client pour un système d'inscription aux cours.
@@ -61,6 +66,11 @@ public class SimpleClient {
                 return;
             }
 
+            client.loadCourses(session);
+
+            // reconnect to the server
+            client = new SimpleClient("localhost", 6000);
+
             System.out.println("1. Consulter les cours offerts pour une autre session");
             System.out.println("2. Inscription à un cours");
             System.out.print("› Choix: ");
@@ -84,12 +94,66 @@ public class SimpleClient {
                 System.out.print("Veuillez saisir le code du cours: ");
                 String courseCode = scanner.nextLine();
 
+                RegistrationForm registrationForm = new RegistrationForm(firstName, lastName, email, matricule,
+                        new Course("IFT1015", courseCode, session));
+
+                client.registerForCourse(registrationForm);
             }
             scanner.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Charge la liste des cours pour une session donnée.
+     * 
+     * @param session Le nom de la session pour laquelle charger les cours.
+     * @throws IOException Si une erreur se produit lors de la communication avec le
+     *                     serveur.
+     */
+    public void loadCourses(String session) throws IOException {
+        try {
+            objectOutputStream.writeObject(Server.LOAD_COMMAND + " " + session);
+            objectOutputStream.flush();
+
+            List<Course> courses = (List<Course>) objectInputStream.readObject();
+
+            System.out.println("Les cours offerts pendant la session d'" + session + " sont :");
+            for (Course course : courses) {
+                System.out.println(" - " + course.getCode() + "        " + course.getName());
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Erreur: la classe Course n'a pas été trouvée.");
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Inscrit l'utilisateur à un cours en utilisant les informations du formulaire
+     * d'inscription.
+     * 
+     * @param registration Les informations d'inscription à un cours.
+     * @throws IOException            Si une erreur se produit lors de la
+     *                                communication avec le serveur.
+     * @throws ClassNotFoundException Si la classe Course n'est pas trouvée.
+     */
+    public void registerForCourse(RegistrationForm registration) throws IOException, ClassNotFoundException {
+
+        System.out.println("Envoi de la demande d'inscription au serveur...: " +
+                registration.toString());
+
+        objectOutputStream.writeObject(Server.REGISTER_COMMAND);
+        objectOutputStream.flush();
+
+        objectOutputStream.writeObject(registration);
+        objectOutputStream.flush();
+
+        String message = (String) objectInputStream.readObject();
+        System.out.println("Message du serveur: " + message);
+        System.out.println(message);
     }
 
     /**
